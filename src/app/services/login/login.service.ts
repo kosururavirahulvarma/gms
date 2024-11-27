78680c93a6cd93b27c94f6d92eb898cf8b2318bc
ravi.kosuru <ravirahulvarma125@gmail.com>
2024-11-27 10:19:30 +0530

    toaster changes

--- src/app/services/login/login.service.ts
+++ src/app/services/login/login.service.ts
@@ -3,12 +3,17 @@ import { inject, Injectable } from '@angular/core';
 import { Loginmodel, User } from '../../model/Loginmodel';
 import { CookieService } from 'ngx-cookie-service';
 import { MatSnackBar } from '@angular/material/snack-bar';
+import { ToastrService, IndividualConfig } from 'ngx-toastr';
 @Injectable({
   providedIn: 'root',
 })
 export class LoginService {
   private snackBar = inject(MatSnackBar);
-  constructor(private http: HttpClient, private cookies: CookieService) {}
+  constructor(
+    private http: HttpClient,
+    private cookies: CookieService,
+    private toastr: ToastrService
+  ) {}
   proceedlogin(_data: Loginmodel) {
     return this.http.get<User[]>(
       'http://localhost:3000/user?id=' +
@@ -17,10 +22,22 @@ export class LoginService {
         _data.password
     );
   }
+  showErrorToast(message: string) {
+    const toastConfig: Partial<IndividualConfig> = {
+      timeOut: 3000, // 3 seconds
+      positionClass: 'toast-top-center',
+      closeButton: true,
+      progressBar: true,
+    };
 
+    this.toastr.error(message, '', toastConfig);
+  }
   isLoggedIn() {
     let isLoggedIn = this.cookies.get('username');
     if (isLoggedIn === '') {
+      this.showErrorToast(
+        'You must be logged in to access this application. Please log in first.'
+      );
       this.snackBar.open(
         'You must be logged in to access this application. Please log in first.',
         'OK',
