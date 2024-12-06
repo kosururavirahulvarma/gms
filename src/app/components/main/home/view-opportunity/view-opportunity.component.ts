9f3970494c53a4ac0c5ddaffeba1f0197030398e
ravi.kosuru <ravirahulvarma125@gmail.com>
2024-12-06 18:39:12 +0530

    back button

--- src/app/components/main/home/view-opportunity/view-opportunity.component.ts
+++ src/app/components/main/home/view-opportunity/view-opportunity.component.ts
@@ -1,33 +1,42 @@
 import { Component } from '@angular/core';
 import { OpportunityEs } from '../../../../model/Opportunity.model';
 import { grantOpportunities } from '../../../../constants/opportunity/opportunity.constants';
-import { ActivatedRoute } from '@angular/router';
+import { ActivatedRoute, NavigationEnd, Router } from '@angular/router';
 import { MatIcon, MatIconModule } from '@angular/material/icon';
-import { FormatDateInWordsPipe } from '../../../../pipes/format-date-in-words.pipe'; 
-import {MatTabsModule} from '@angular/material/tabs';
+import { FormatDateInWordsPipe } from '../../../../pipes/format-date-in-words.pipe';
+import { MatTabsModule } from '@angular/material/tabs';
 
 @Component({
   selector: 'app-view-opportunity',
   standalone: true,
   imports: [MatIcon, MatIconModule, FormatDateInWordsPipe, MatTabsModule],
   templateUrl: './view-opportunity.component.html',
-  styleUrl: './view-opportunity.component.scss'
+  styleUrl: './view-opportunity.component.scss',
 })
 export class ViewOpportunityComponent {
   opportunities: OpportunityEs[] = grantOpportunities;
   opportunityNumber: any;
-  data:any;
-  constructor(private route: ActivatedRoute) {}
+  data: any;
+  constructor(private route: ActivatedRoute,private router :Router) {}
 
   ngOnInit() {
-    this.route.paramMap.subscribe(params => {
+    this.route.paramMap.subscribe((params) => {
       this.opportunityNumber = params.get('opportunity_number');
-  });
-  this.opportunities.forEach(element => {
-    if(element._source.opportunity_number === this.opportunityNumber){
-      this.data = element._source
-    }
-  });
-}
-
+    });
+    this.opportunities.forEach((element) => {
+      if (element._source.opportunity_number === this.opportunityNumber) {
+        this.data = element._source;
+      }
+    });
+    this.router.events.subscribe(event => {
+      if (event instanceof NavigationEnd) {
+        // Scroll to the top of the page
+        window.scrollTo(0, 0);
+      }
+    });
+  }
+  backToResults(){
+      
+      this.router.navigate(['home/opportunity']);
+  }
 }
